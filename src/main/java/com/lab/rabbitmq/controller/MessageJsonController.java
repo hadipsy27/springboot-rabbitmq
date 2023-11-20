8aaec7b74d326e36dbfb92b9188279c048aa550a
hadipsy27 <hadipsy27@gmail.com>
2023-11-20 21:18:13 +0700

    256. Create REST API to Send JSON Object

---
+++ src/main/java/com/lab/rabbitmq/controller/MessageJsonController.java
@@ -0,0 +1,26 @@
+package com.lab.rabbitmq.controller;
+
+import com.lab.rabbitmq.dto.User;
+import com.lab.rabbitmq.publisher.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer rabbitMQJsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer rabbitMQJsonProducer) {
+        this.rabbitMQJsonProducer = rabbitMQJsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        rabbitMQJsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("JSON Message Sent to RabbitMQ...");
+    }
+}
