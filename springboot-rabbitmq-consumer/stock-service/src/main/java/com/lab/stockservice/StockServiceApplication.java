c321ab186a92e25e3c083a36ff806960b26daf16
hadipsy27 <hadipsy27@gmail.com>
2023-11-28 14:16:22 +0700

    265. StockService Microservice - Configure RabbitMQ

--- springboot-rabbitmq-consumer/stock-service/src/main/java/com/lab/stockservice/StockServiceApplication.java
+++ springboot-rabbitmq-consumer/stock-service/src/main/java/com/lab/stockservice/StockServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StockServiceApplication {
+	// Stock Service going to read Order event from RabbitMQ
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockServiceApplication.class, args);
